0c45303c08b55a8b09880a4b5c038d492f351644
Ivana Mijatovic <ivanamijatovic002@gmail.com>
2026-03-09 18:31:53 +0100

    Dodata jednostavna todo aplikacija sa dodaj i obrisi funkcionalnostima

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('todo');
+export class AppComponent {
+  task: string = '';
+  tasks: string[] = [];
+
+  addTask() {
+    if (this.task.trim()) {
+      this.tasks.push(this.task.trim());
+      this.task = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
